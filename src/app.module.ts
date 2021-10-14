1d2921145f4b1e6a003d44d992adba4717fe0205
Epro <Praktikant.Epro@emerson.com>
2021-10-14 16:54:39 +0200

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MineService } from './mine/mine.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MineService],
 })
 export class AppModule {}
