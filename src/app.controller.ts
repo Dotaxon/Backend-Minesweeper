4ff1c6305712460b079e9ae72e4bd7c065200be7
Epro <Praktikant.Epro@emerson.com>
2021-10-15 12:14:25 +0200

    Added Functions to create a MineArray when a http Request is made

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { ConsoleLogger, Controller, Get, Query } from '@nestjs/common';
+import { query } from 'express';
 import { AppService } from './app.service';
+import { MineService } from './mine/mine.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private mineService: MineService,
+    ) {}
 
-  @Get()
+  @Get('/hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get()
+  getMineArray(@Query('rows') rows, @Query('columns') columns, @Query('mines') mines): boolean[][]{
+    console.log("yessss")
+    return this.mineService.getMineArray(rows, columns, mines);
   }
 }
--- src/mine/mine.service.ts
+++ src/mine/mine.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MineService {}
+export class MineService {
+
+
+    getMineArray(rows: number, columns:number, mines: number) : boolean[][]{
+        
+        if(mines >= rows*columns) return [];
+        let arr_Mines : boolean[][] = [];
+    
+
+        //initialization of arr_Mines
+        for (let i = 0; i < rows; i++) {
+            arr_Mines[i] = [];
+            for (let j = 0; j < columns; j++) {
+                arr_Mines[i][j] = false;   
+            }
+        }
+    
+
+        //Genrate Random Cordinates at wich place the array will be true
+        //if the positon is already true it will generate new Cordinates as long as necessary
+        for (let i = 0 ; i < mines; i++){
+            let xCord : number;
+            let yCord : number;
+
+            do {
+                xCord = this.getRandomInt(0,columns);
+                yCord = this.getRandomInt(0,rows);
+
+            } while (arr_Mines[yCord][xCord] != false);
+
+            arr_Mines[yCord][xCord] = true;
+        }
+    
+    
+        return arr_Mines;
+    }
+
+
+    /**Generates A random number between min(inclusiv) and max(exclusiv)
+     * 
+     * @param min minimal value (inclusiv)
+     * @param max maximal value (exclusiv)
+     * @returns Value >= min and value < max
+     */
+    getRandomInt(min : number, max : number) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+}
